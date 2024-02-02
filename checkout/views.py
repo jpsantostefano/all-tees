ed328ab340883abef7dc6561864f68cbe92e8a45
jpsantostefano <jpsantostefano@gmail.com>
2024-02-02 19:54:51 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OXAoLAG5VMJODD2HddnY1ooccK8T9ZbOVk1Lp1GIk0aOs9E9E0t1tjZU1xBn3j81ifU1A0D68DCkGoxnbxrncme00EkECoID1',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
