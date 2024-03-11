21613c659ee1af2160b968e13fd948d9c7913612
jpsantostefano <jpsantostefano@gmail.com>
2024-03-11 20:35:17 +0000

    careers model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Careers(models.Model):
+    full_name = models.CharField(max_length=100)
+    position_choice = [
+        ('Marketing', 'Marketing'),
+        ('Sales', 'Sales'),
+        ('Customer Service', 'Customer Service'),
+    ]
+    position = models.CharField(max_length=20, choices=position_choice)
+    cv = models.FileField(upload_to='cv_uploads/', blank=True, null=True)
+    message = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.full_name
