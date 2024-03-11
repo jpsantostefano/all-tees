ab19cd692aaf4a85fac660d7fdb7b79ac57b5fdf
jpsantostefano <jpsantostefano@gmail.com>
2024-03-11 21:09:36 +0000

    careers section finished

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import CareersForm
+from django import forms
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -8,7 +11,16 @@ def about_us(request):
     return render(request, 'home/more/about_us.html')
 
 def careers(request):
-    return render(request, 'home/more/careers.html')
+    if request.method == 'POST':
+        form = CareersForm(request.POST, request.FILES)
+        # Save on database
+        if form.is_valid():
+            career_instance = form.save()
+            messages.success(request, "Form submitted successfully. Thanks for applying!")
+            return redirect('careers')
+    else:
+        form = CareersForm()
+    return render(request, 'home/more/careers.html', {'form': form})
 
 def news(request):
     return render(request, 'home/more/news.html')
