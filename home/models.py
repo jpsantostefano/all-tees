8c3cd1c4702d749e1220d82b5191d842e026b600
jpsantostefano <jpsantostefano@gmail.com>
2024-03-12 10:59:35 +0000

    post news model created

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
+from django.db.models.signals import post_save
 
-# Create your models here.
+def current_date():
+    return datetime.today().isoformat()
 
+# Careers
 class Careers(models.Model):
     full_name = models.CharField(max_length=100)
     position_choice = [
@@ -16,3 +21,14 @@ class Careers(models.Model):
 
     def __str__(self):
         return self.full_name
+
+# News
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    date = models.DateField(default=current_date)
+    body = models.TextField()
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+
+    def __self__(self):
+        return self.title
