a3a6c18881e592121733c29b125d08138af4cb98
jpsantostefano <jpsantostefano@gmail.com>
2024-03-22 15:26:21 +0000

    removed secret key

--- all_tees/settings.py
+++ all_tees/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jpg9*pth93)it^$fwc!g@$oi+qh+a)&#9s1&1w*cp#i&3mv3k'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-jpsantostefano-alltees-8o0zweijybj.ws-eu110.gitpod.io','all-tees-20b457acc74a.herokuapp.com', 'localhost']
 
